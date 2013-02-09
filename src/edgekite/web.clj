a78aed381f305160524a37bcd3b1159dc1a247d6
Matt Lee <thatismatt@gmail.com>
2013-02-09 23:11:09 +0000

    Added URL segmentation. Added simple guestbook.

--- src/edgekite/web.clj
+++ src/edgekite/web.clj
@@ -1,6 +1,13 @@
 (ns edgekite.web
-  (:use [hiccup.core]
+  (:use [clojure.string :only [split]]
+        [ring.middleware.params]
+        [hiccup.core]
         [hiccup.page]))
+
+(def state (atom {}))
+
+(defn store [k v]
+  (swap! state #(assoc % k v)))
 
 (defn css [req]
   {:status 200
@@ -26,9 +33,16 @@
       (comment [:script "setTimeout(\"location = location\", 1000)"])]])))
 
 (defn home [req]
+  (let [name ((:params req) "name" "")]
+    (store name (java.util.Date.))
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (page "edgekite" (str "Welcome " name " to edgekite."))}))
+
+(defn guests [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (page "edgekite" "Welcome to edgekite.")})
+   :body (page "guests" (map #(do [:div (first %) " - " (second %)]) (seq @state)))})
 
 (defn four-oh-four [req]
   {:status 404
@@ -37,8 +51,21 @@
 
 (def routes
   {"/" home
+   "/guests" guests
    "/style.css" css})
 
-(defn handler [req]
+(defn router [req]
   (let [r (routes (:uri req) four-oh-four)]
     (r req)))
+
+(defn wrap-segment-uri
+  [handler]
+  (fn [request]
+    (let [segs (rest (split (:uri request) #"/"))
+          req (assoc request :segments segs)]
+      (handler req))))
+
+(def handler
+  (-> router
+      wrap-params
+      wrap-segment-uri))
