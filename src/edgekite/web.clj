3b645a2355f94ca0e669577dc091f13f89f66006
Matt Lee <thatismatt@gmail.com>
2013-02-09 21:31:39 +0000

    Add 404 page. Factor out styles in to separate file. Add auto reload for dev time.

--- src/edgekite/web.clj
+++ src/edgekite/web.clj
@@ -2,26 +2,43 @@
   (:use [hiccup.core]
         [hiccup.page]))
 
-(def css "body { font-family: monospace; margin: 0; padding: 0; color: #6f6950; }
-          h1 { padding: 0; margin: 0; }
-          #top-bar { padding: 5px 0; }
-          #wrapper { margin: 0 auto; width: 800px; }
-          #content { background: #d5d0cc; padding: 0 5px; min-height: 500px;  }")
+(defn css [req]
+  {:status 200
+   :headers {"Content-Type" "text/css"}
+   :body "body { font-family: monospace; margin: 0; padding: 0; color: #6f6950; }
+   h1 { padding: 0; margin: 0; }
+   #top-bar { padding: 5px 0; }
+   #wrapper { margin: 0 auto; width: 800px; }
+   #content { background: #d5d0cc; padding: 0 5px; min-height: 500px; }"})
 
-(defn page [body]
-  (html (html5
-         [:html
-          [:head
-           [:title "edgekite"]
-           [:style css]]
-          [:body
-           [:div#wrapper
-            [:div#content
-             [:div#top-bar
-              [:h1 "edgekite"]]
-             body]]]])))
+(defn page [title body]
+  (html (include-css "/style.css")
+   (html5
+    [:html
+     [:head [:title "edgekite"]]
+     [:body
+      [:div#wrapper
+       [:div#content
+        [:div#top-bar
+         [:h1 title]]
+        body
+        (comment [:div (java.util.Date.)])]]
+      (comment [:script "setTimeout(\"location = location\", 1000)"])]])))
 
-(defn handler [req]
+(defn home [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (page "Welcome to edgekite.")})
+   :body (page "edgekite" "Welcome to edgekite.")})
+
+(defn four-oh-four [req]
+  {:status 404
+   :headers {"Content-Type" "text/html"}
+   :body (page "Four, Oh! Four." "Errrm...")})
+
+(def routes
+  {"/" home
+   "/style.css" css})
+
+(defn handler [req]
+  (let [r (routes (:uri req) four-oh-four)]
+    (r req)))
