bf1af0c8af328dd52b21b8fa65fbdb468a27bbf8
Matt Lee <thatismatt@gmail.com>
2013-03-19 19:32:41 +0000

    Add device width meta tag.

--- src/edgekite/view.clj
+++ src/edgekite/view.clj
@@ -13,6 +13,7 @@
     [:html
      [:head
       [:title "edgekite"]
+      [:meta {:content "width=device-width, initial-scale=1.0" :name "viewport"}]
       (include-css (gu :style))]
      [:body
       [:div#wrapper
