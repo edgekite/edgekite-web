178b5fa276e9f316fec2f8193badc6d53b949c09
Matt Lee <thatismatt@gmail.com>
2013-03-22 12:08:01 +0000

    Upgrade to clojure 1.5.0.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://edgekite.com/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [ring "1.2.0-beta1"]
                  [hiccup "1.0.2"]
                  [gudu "0.1.0-SNAPSHOT"]]
